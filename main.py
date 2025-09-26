ce49be12e3b7bd0d7c36fbfe58d6a0ffc10b575e
Lucas Ferreira <lucasoferreira43@gmail.com>
2025-09-26 10:59:13 -0300

    7. Executando Todos os Testes

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from TestFramework import TestCaseTest, TestResult, TestSuite, TestSuiteTest, TestLoader, TestLoaderTest, TestRunner
+from TestFramework import TestCaseTest, TestSuite, TestSuiteTest, TestLoader, TestLoaderTest, TestRunner
 
 loader = TestLoader()
-suite = loader.make_suite(TestLoaderTest)
+test_case_suite = loader.make_suite(TestCaseTest)
+test_suite_suite = loader.make_suite(TestSuiteTest)
+test_load_suite = loader.make_suite(TestLoaderTest)
+
+suite = TestSuite()
+suite.add_test(test_case_suite)
+suite.add_test(test_suite_suite)
+suite.add_test(test_load_suite)
 
 runner = TestRunner()
 runner.run(suite)
